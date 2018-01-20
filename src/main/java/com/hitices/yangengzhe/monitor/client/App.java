539b5acc2c045982a33df73dda805352ee7a66bc
yangengzhe <emailvip@vip.qq.com>
2018-01-20 13:23:57 +0800

    init

---
+++ src/main/java/com/hitices/yangengzhe/monitor/client/App.java
@@ -0,0 +1,13 @@
+package com.hitices.yangengzhe.monitor.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
